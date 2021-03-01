41ae784e2c400936cd51ba1acb82fb515071ad83
Michał Leśniewski <mlesniew@gmail.com>
2021-03-01 21:15:51 +0100

    Add NTP client

--- src/calor.cpp
+++ src/calor.cpp
@@ -1,11 +1,13 @@
 #include <Arduino.h>
 
-#include <OneWire.h>
 #include <DallasTemperature.h>
+#include <NTPClient.h>
+#include <OneWire.h>
+#include <WiFiUdp.h>
 
 #include "led.h"
-#include "wifi_control.h"
 #include "shift_register.h"
+#include "wifi_control.h"
 #include "wifi_readings.h"
 
 #define DS18B20_RESOLUTION 12
@@ -23,6 +25,9 @@ ShiftRegister<1> shift_register(
         D0,  // latch pin
         (uint8_t[1]){ 0b00001111, }  // inverted outputs
         );
+
+WiFiUDP ntp_udp;
+NTPClient ntp_client(ntp_udp, 60 * 60);
 
 bool setup_sensors() {
     DeviceAddress address;
@@ -72,6 +77,7 @@ void setup() {
 
     shift_register.init();
     wifi_control.init();
+    ntp_client.begin();
 
 #if 0
     if (!setup_sensors()) {
@@ -84,6 +90,13 @@ void setup() {
 void loop() {
     Serial.println(WiFi.localIP());
     wifi_readings.load("http://192.168.1.200/measurements.json");
+
+    static bool have_time = false;
+    have_time = have_time || ntp_client.update();
+
+    if (have_time) {
+        Serial.println(ntp_client.getFormattedTime());
+    }
 
 #if 0
     sensors.requestTemperatures();
